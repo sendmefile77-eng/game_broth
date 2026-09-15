db069e8c10a76e2d93513bc17e5e16ebc1cc4ba8
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-15 23:55:55 +0300

    Bump Game Broth to 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sendmefile77.gamebroth"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
     }
 
     buildFeatures { compose = true }

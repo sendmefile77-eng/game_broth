2cb6fd62a6e835f134653e1d46bae98ed87e8752
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-16 00:57:25 +0300

    Bump day ambience fix to 0.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sendmefile77.gamebroth"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.4.1"
+        versionCode = 7
+        versionName = "0.4.2"
     }
 
     buildFeatures { compose = true }

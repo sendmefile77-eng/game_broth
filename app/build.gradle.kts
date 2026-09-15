f32f6364a1fd1980a47a97f2bcf003bb8b654eb1
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-16 00:31:57 +0300

    Bump image identity fix to 0.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sendmefile77.gamebroth"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.4.0"
+        versionCode = 6
+        versionName = "0.4.1"
     }
 
     buildFeatures { compose = true }

5cf818ecb4dc78c8365da8555e93450b75124d97
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-16 12:13:28 +0300

    Bump living staff pass to 0.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sendmefile77.gamebroth"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.6.0"
+        versionCode = 14
+        versionName = "0.7.0"
     }
 
     buildFeatures { compose = true }

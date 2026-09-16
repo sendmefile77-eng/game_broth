50185c754df1ebf0925427b796119b446b0b0326
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-16 10:57:32 +0300

    Bump app to 0.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sendmefile77.gamebroth"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.5.1"
+        versionCode = 12
+        versionName = "0.5.2"
     }
 
     buildFeatures { compose = true }

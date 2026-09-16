141ad23357cc2b2d7246ce6446dc634c5dd8dfb9
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-16 12:43:51 +0300

    Bump release candidate to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sendmefile77.gamebroth"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.7.0"
+        versionCode = 15
+        versionName = "1.0.0"
     }
 
     buildFeatures { compose = true }

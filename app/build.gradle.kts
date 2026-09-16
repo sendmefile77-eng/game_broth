d35d58cbdd3435cba7b515f17f4db330e97260cd
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-16 10:17:39 +0300

    Bump version for image prompt architecture refactor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sendmefile77.gamebroth"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.4.4"
+        versionCode = 10
+        versionName = "0.5.0"
     }
 
     buildFeatures { compose = true }

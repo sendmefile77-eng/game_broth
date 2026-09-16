ec51a8b847dc2d4c62539e84e3e07e7db26273d7
sendmefile77-eng <sendmefile77@gmail.com>
2026-09-16 08:45:14 +0300

    Bump recruit portrait hotfix to 0.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sendmefile77.gamebroth"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.4.3"
+        versionCode = 9
+        versionName = "0.4.4"
     }
 
     buildFeatures { compose = true }
